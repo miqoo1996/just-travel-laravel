d0ccffc1c97f5318e380ee77aaa099e1ec45317b
miqayel.ishkhanyan <miqayel.ishkhanyan@ifixgroup.com>
2017-06-21 11:46:00 +0400

    page order images

--- resources/views/admin/tour_orders.blade.php
+++ resources/views/admin/tour_orders.blade.php
@@ -21,42 +21,17 @@
                 <div id="sortables" class="col-md-10 col-md-offset-1 tours">
                     <ul data-ajax="{{route('admin-tour-items-orders')}}" id="sortable-tour" class="col-md-12">
                         @foreach($tours as $tour)
-                            @if($tour->isDaily())
-                                <li data-page-id="{{ $tour['tour_id'] }}" class="item">
-                                    <a href="javascript:void(0)">
-                                        <img src="{{App\SimpleImage::image($tour['tour_main_image'], true)}}">
-                                        <span class="tour-title">{{$tour['tour_name_en']}}</span>
-                                    </a>
-                                    <div class="tour-data">
-                                        @if($tour['basic_frequency'])
-                                            <div class="frequency">
-                                                @foreach(config('const.week_days_'.app()->getLocale()) as $wd => $short)
-                                                    @if(strpos($tour['basic_frequency'], $wd) !== false)
-                                                        <span class="freq-day available">{{$short}}</span>
-                                                    @else
-                                                        <span class="freq-day">{{$short}}</span>
-                                                    @endif
-                                                @endforeach
-                                            </div>
-                                            <div class="price {{$currency['currency']}}"> {{round($tour['basic_price_adult'] / $currency[$currency['currency']], 2)}}</div>
-                                        @else
-                                            <div class="price {{$currency['currency']}}">{{round($tour['double_adult'] / $currency[$currency['currency']], 2)}}</div>
-                                        @endif
-                                    </div>
-                                </li>
-                            @else
-                                <li data-page-id="{{ $tour['tour_id'] }}" class="item">
-                                    <a href="tours/{{$tour['tour_url']}}" class="tour-photo">
-                                        <img src="{{App\SimpleImage::image($tour['tour_main_image'], true)}}">
-                                        <span class="tour-title">{{$tour['tour_name_en']}}</span>
-                                    </a>
-                                    <div class="tour-data">
-                                        <div class="tourdate">{{str_replace('/','.',$tour['date'])}}</div>
-                                        <div class="price {{$currency['currency']}}">{{round($tour['single_adult'] / $currency[$currency['currency']], 2)}}</div>
-                                        <div class="clear"></div>
-                                    </div>
-                                </li>
-                            @endif
+                            <li data-page-id="{{ $tour['tour_id'] }}" class="item">
+                                <a href="tours/{{$tour['tour_url']}}" class="tour-photo">
+                                    <img src="{{App\SimpleImage::image($tour['tour_main_image'], true)}}">
+                                    <span class="tour-title">{{$tour['tour_name_en']}}</span>
+                                </a>
+                                <div class="tour-data">
+                                    <div class="tourdate">{{str_replace('/','.',$tour['date'])}}</div>
+                                    <div class="price {{$currency['currency']}}">-- price --</div>
+                                    <div class="clear"></div>
+                                </div>
+                            </li>
                         @endforeach
                     </ul>
                 </div>
