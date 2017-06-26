e9a00e0832b61fe83d06fa74e4496f01457ef818
miqayel.ishkhanyan <miqayel.ishkhanyan@ifixgroup.com>
2017-06-26 16:01:45 +0400

    clone hotel html

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,4 @@
-$(document).ready( function () {
+$(document).ready(function () {
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -8,85 +8,86 @@ $(document).ready( function () {
     dragDrop();
     toggleAreas();
     addHotel();
-    $('.add_day').on("click", function(){
+    $('.add_day').on("click", function () {
         var container_en = $('#custom_day_container_en'),
             childrenCount = $(container_en).children().length,
             newCount = childrenCount + 1;
 
-        $(container_en).append("<div class='custom_day'>"+
-                                    "<label class='control-label col-md-3 col-sm-3 col-xs-12'>Day " + newCount+ "</label>"+
-                                    "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10'>"+
-                                        "<input type='text' class='form-control input-medium' name='custom_day_title_en[]' placeholder='title'>" +
-                                    "</div>"+
-                                    "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10 col-md-offset-3 col-sm-offset-3'>"+
-                                        "<textarea class='resizable_textarea form-control' placeholder='' name='custom_day_desc_en[]'></textarea>"+
-                                    "</div>" +
-                                    "<div class='clearfix'></div>" +
-                                "</div>");
+        $(container_en).append("<div class='custom_day'>" +
+            "<label class='control-label col-md-3 col-sm-3 col-xs-12'>Day " + newCount + "</label>" +
+            "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10'>" +
+            "<input type='text' class='form-control input-medium' name='custom_day_title_en[]' placeholder='title'>" +
+            "</div>" +
+            "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10 col-md-offset-3 col-sm-offset-3'>" +
+            "<textarea class='resizable_textarea form-control' placeholder='' name='custom_day_desc_en[]'></textarea>" +
+            "</div>" +
+            "<div class='clearfix'></div>" +
+            "</div>");
 
         var container_ru = $('#custom_day_container_ru');
 
-        $(container_ru).append("<div class='custom_day'>"+
-                                    "<label class='control-label col-md-3 col-sm-3 col-xs-12'>Day " + newCount+ "</label>"+
-                                    "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10'>"+
-                                        "<input type='text' class='form-control input-medium' name='custom_day_title_ru[]' placeholder='title'>" +
-                                    "</div>"+
-                                    "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10 col-md-offset-3 col-sm-offset-3'>"+
-                                        "<textarea class='resizable_textarea form-control' placeholder='' name='custom_day_desc_ru[]'></textarea>"+
-                                    "</div>" +
-                                    "<div class='clearfix'></div>" +
-                                "</div>");
+        $(container_ru).append("<div class='custom_day'>" +
+            "<label class='control-label col-md-3 col-sm-3 col-xs-12'>Day " + newCount + "</label>" +
+            "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10'>" +
+            "<input type='text' class='form-control input-medium' name='custom_day_title_ru[]' placeholder='title'>" +
+            "</div>" +
+            "<div class='col-md-9 col-sm-9 col-xs-12 margin-b-10 col-md-offset-3 col-sm-offset-3'>" +
+            "<textarea class='resizable_textarea form-control' placeholder='' name='custom_day_desc_ru[]'></textarea>" +
+            "</div>" +
+            "<div class='clearfix'></div>" +
+            "</div>");
 
     });
-    $('.remove_day').on("click", function(){
+    $('.remove_day').on("click", function () {
         var container_en = $('#custom_day_container_en'),
             childrenCount_en = $(container_en).children().length;
-        if(childrenCount_en > 2){
+        if (childrenCount_en > 2) {
             $(container_en).children().last().remove();
         }
         var container_ru = $('#custom_day_container_ru'),
-        childrenCount_ru = $(container_ru).children().length;
-        if(childrenCount_ru > 2){
+            childrenCount_ru = $(container_ru).children().length;
+        if (childrenCount_ru > 2) {
             $(container_ru).children().last().remove();
         }
     });
 
-    $("input[type='radio'][name=custom_day_prp]").on('change', function(){
-        if($("input[type='radio'][name=custom_day_prp]:checked").val() == 'any'){
+    $("input[type='radio'][name=custom_day_prp]").on('change', function () {
+        if ($("input[type='radio'][name=custom_day_prp]:checked").val() == 'any') {
             $('.datepicker-container').hide();
         } else {
             $('.datepicker-container').show();
         }
     });
 
-    $('#remove_hotel').on("click", function(){
+    $('#remove_hotel').on("click", function () {
         var container = $('.hotel-container'),
             childrenCount = $(container).children().length;
-        if(childrenCount > 1){
+        if (childrenCount > 1) {
             $(container).children().last().remove();
 
         }
     });
-    $(".tour-category-checkboxes input[type='checkbox'].basic").change(function(){
+    $(".tour-category-checkboxes input[type='checkbox'].basic").change(function () {
         toggleAreas();
     });
 
-    $('.custom-image-remove-button').on('click', function(){
+    $('.custom-image-remove-button').on('click', function () {
         var container = $(this).parent();
         var attributes = $(this).attr('id');
         var attrArray = attributes.split('?');
         $.ajax({
             url: '/admin/remove-image',
             type: 'POST',
-            data: { 'type'          : attrArray[0],
-                    'id'            : attrArray[1],
-                    'image_type'    : attrArray[2],
-                    'image_position': attrArray[3]
+            data: {
+                'type': attrArray[0],
+                'id': attrArray[1],
+                'image_type': attrArray[2],
+                'image_position': attrArray[3]
             },
             success: function (data) {
                 console.log(data);
-                if(data == 'true'){
-                    $(container).fadeOut( "slow", function() {
+                if (data == 'true') {
+                    $(container).fadeOut("slow", function () {
                         $(container).parent('li').hide('slow');
                         $(container).parent('li').remove();
                     });
@@ -94,24 +95,24 @@ $(document).ready( function () {
                     console.log('error')
                 }
             },
-            error: function(error){
+            error: function (error) {
                 $('body').html(error);
             }
         });
 
     });
     var d, container;
-    $(".remove").on("click", function(){
+    $(".remove").on("click", function () {
         $('#item-name').text($(this).data('name'));
         d = $(this).attr('id').split('/');
-        container = $('#cnt-'+ d[0]);
-        $("#cancel").on("click", function(){
+        container = $('#cnt-' + d[0]);
+        $("#cancel").on("click", function () {
             d = null;
             container = null;
         });
-        $("#confirm").on("click", function(){
+        $("#confirm").on("click", function () {
             $.ajax({
-                url:'/admin/remove-data',
+                url: '/admin/remove-data',
                 type: 'POST',
                 data: {
                     'id': d[0],
@@ -119,14 +120,14 @@ $(document).ready( function () {
                 },
 
                 success: function (data) {
-                    if(data == 'true'){
+                    if (data == 'true') {
                         $('#delete_modal').modal('hide');
-                        $(container).fadeOut( "slow", function() {
+                        $(container).fadeOut("slow", function () {
                             $(container).remove();
                         });
                     }
                 },
-                error: function(error){
+                error: function (error) {
                     $('body').html(error);
                 }
             });
@@ -136,10 +137,10 @@ $(document).ready( function () {
 
 });
 
-function toggleAreas(){
+function toggleAreas() {
     var c = $('.tour-category-checkboxes input.custom');
     var checked = $("input[type='checkbox'].basic:checked").length;
-    if(checked > 0){
+    if (checked > 0) {
         $(c).attr('checked', false);
         $(c).attr('disabled', 'disabled');
         $('.basic-field').show();
@@ -156,31 +157,33 @@ var files;
 
 function dragDrop() {
     var $form = $('.admin-image-label');
-    $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+    $form.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
         e.preventDefault();
         e.stopPropagation();
     })
-        .on('dragover dragenter', function() {
+        .on('dragover dragenter', function () {
             $form.addClass('is-dragover');
         })
-        .on('dragleave dragend drop', function() {
+        .on('dragleave dragend drop', function () {
             $form.removeClass('is-dragover');
         })
-        .on('drop', function(e) {
+        .on('drop', function (e) {
             droppedFiles = e.originalEvent.dataTransfer.files;
             $form.children('.admin-image-upload').prop('files', droppedFiles);
             // console.log(droppedFiles);
         })
-        .on("change", '.admin-image-upload', function(){
+        .on("change", '.admin-image-upload', function () {
             runReadingUrl($form, this);
             stopLoading();
         })
-        .on('click','#clear-images', function (e) {
+        .on('click', '#clear-images', function (e) {
             e.preventDefault();
             e.stopPropagation();
             $form.children('input.admin-image-upload').attr('type', '');
             $form.children('.admin-image-upload').attr('type', 'file');
-            $('.drop-image').fadeOut(400, function(){ $(this).remove();});
+            $('.drop-image').fadeOut(400, function () {
+                $(this).remove();
+            });
         });
 
     // var par = $form.children('.admin-image-upload');
@@ -189,10 +192,10 @@ function dragDrop() {
     // });
 }
 
-    function readURL(input) {
-        $('.admin-image-label').addClass('loading');
-        if (input.files && input.files[0]) {
-        var counter = 0 ;
+function readURL(input) {
+    $('.admin-image-label').addClass('loading');
+    if (input.files && input.files[0]) {
+        var counter = 0;
         var name;
         var fileCount;
         var newData = '';
@@ -202,16 +205,16 @@ function dragDrop() {
             name = this.name;
             reader.onload = function () {
                 // $('.admin-image-label').append("<div id='d_i_" + counter +"' class='drop-image' style='display:none'><span data-id='"+ name +"'></span><img src='" + e.target.result + "'></div>");
-                $('.admin-image-label').append("<div id='d_i_" + counter +"' class='drop-image'><img src='" + this.result + "'></div>")
+                $('.admin-image-label').append("<div id='d_i_" + counter + "' class='drop-image'><img src='" + this.result + "'></div>")
             };
             counter++;
             reader.readAsDataURL(this);
         });
-            $(input.files).promise().done(function() {
-                console.log(newData);
-                $('.admin-image-label').addClass('finished');
-            });
-        }
+        $(input.files).promise().done(function () {
+            console.log(newData);
+            $('.admin-image-label').addClass('finished');
+        });
+    }
 }
 
 function runReadingUrl($form, $this) {
@@ -221,30 +224,31 @@ function runReadingUrl($form, $this) {
 function stopLoading() {
     $('.admin-image-label').removeClass('loading');
 }
-    var hotel =  "<div class='new_hotel'><div class='col-md-3 col-sm-3 col-xs-12'> Hotel" +
-        "<select class='form-control qaq' name='hotel[hotel_id][]'>"+
-        "</select></div>" +
-        "<div class='col-md-3 col-sm-3 col-xs-12'> Single Adult (12-99) <span class='required'>*</span>" +
-        "<input type='text' class='form-control' placeholder='Price' name='hotel[single_adult][]' value=''></div>" +
-        "<div class='col-md-3 col-sm-3 col-xs-12'> Double Adult (12-99) <span class='required'>*</span>" +
-        "<input type='text' class='form-control' placeholder='Price' name='hotel[double_adult][]' value=''></div>" +
-        "<div class='col-md-3 col-sm-3 col-xs-12'> Tripple Adult (12-99) <span class='required'>*</span>" +
-        "<input type='text' class='form-control' placeholder='Price' name='hotel[triple_adult][]' value=''></div>" +
-        "<div class='col-md-3 col-sm-3 col-xs-12'> </div>" +
-        "<div class='col-md-3 col-sm-3 col-xs-12'> Child (4-11) <span class='required'>*</span>" +
-        "<input type='text' class='form-control' placeholder='Price' name='hotel[child][]' value=''></div>" +
-        "<div class='col-md-3 col-sm-3 col-xs-12'> Infant (0-4) <span class='required'>*</span>" +
-        "<input type='text' class='form-control' placeholder='Price' name='hotel[infant][]' value=''></div>" +
-        "<div class='clearfix margin-b-10'></div></div>";
+var hotel = "<div class='new_hotel'><div class='col-md-3 col-sm-3 col-xs-12'> Hotel" +
+    "<select class='form-control qaq' name='hotel[hotel_id][]'>" +
+    "</select></div>" +
+    "<div class='col-md-3 col-sm-3 col-xs-12'> Single Adult (12-99) <span class='required'>*</span>" +
+    "<input type='text' class='form-control' placeholder='Price' name='hotel[single_adult][]' value=''></div>" +
+    "<div class='col-md-3 col-sm-3 col-xs-12'> Double Adult (12-99) <span class='required'>*</span>" +
+    "<input type='text' class='form-control' placeholder='Price' name='hotel[double_adult][]' value=''></div>" +
+    "<div class='col-md-3 col-sm-3 col-xs-12'> Tripple Adult (12-99) <span class='required'>*</span>" +
+    "<input type='text' class='form-control' placeholder='Price' name='hotel[triple_adult][]' value=''></div>" +
+    "<div class='col-md-3 col-sm-3 col-xs-12'> </div>" +
+    "<div class='col-md-3 col-sm-3 col-xs-12'> Child (4-11) <span class='required'>*</span>" +
+    "<input type='text' class='form-control' placeholder='Price' name='hotel[child][]' value=''></div>" +
+    "<div class='col-md-3 col-sm-3 col-xs-12'> Infant (0-4) <span class='required'>*</span>" +
+    "<input type='text' class='form-control' placeholder='Price' name='hotel[infant][]' value=''></div>" +
+    "<div class='clearfix margin-b-10'></div></div>";
 
 function addHotel() {
     var addHotel = $('#add_hotel');
-    $(addHotel).unbind('click').bind('click', function(){
-        var container = $('.hotel-container').first();
-        var select = $(container).find('.new_hotel').last().find('select');
+    $(addHotel).unbind('click').bind('click', function () {
+        var containerFirst = $('.hotel-container').first();
+        var containerLast = $('.hotel-container').last();
+        var select = $(containerFirst).find('.new_hotel').last().find('select');
         var freeHotels = $(select).children('option');
-        $(container).append(hotel);
-        var newSelect =  $(container).find('.new_hotel').last().find('select');
+        $(containerLast).append(hotel);
+        var newSelect = $(containerLast).find('.new_hotel').last().find('select');
         $(newSelect).append(freeHotels.clone());
     });
 }
